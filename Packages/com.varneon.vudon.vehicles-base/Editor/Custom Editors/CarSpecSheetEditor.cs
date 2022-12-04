c936f6d5f9396b933edd4ce873f91f187856cd75
Varneon <me@varneon.com>
2022-12-04 13:33:52 -0800

    Add car spec sheet scriptable object

---
+++ Packages/com.varneon.vudon.vehicles-base/Editor/Custom Editors/CarSpecSheetEditor.cs
@@ -0,0 +1,124 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Varneon.VUdon.VehiclesBase.DataPresets.Editor
+{
+    [CustomEditor(typeof(CarSpecSheet))]
+    public class CarSpecSheetEditor : UnityEditor.Editor
+    {
+        private CarSpecSheet specSheet;
+
+        private CarSpecSheet.CarSpecSheetData specSheetData;
+
+        private AnimationCurve engineTorqueCurve;
+
+        private static readonly GUIContent
+            NameFieldContent = new GUIContent("Name", "Name of the car preset"),
+            DescriptionFieldContent = new GUIContent("Description", "Description of the car preset"),
+            MakeFieldContent = new GUIContent("Make", "Make of the car"),
+            ModelFieldContent = new GUIContent("Model", "Model of the car"),
+            YearFieldContent = new GUIContent("Year", "Production year of the car"),
+            WeightFieldContent = new GUIContent("Weight", "Default operational weight of the car"),
+            TopSpeedFieldContent = new GUIContent("Top Speed", "Top speed of the car (km/h)"),
+            MaxEngineTorqueFieldContent = new GUIContent("Max Engine Torque", "Maximum torque of the engine in Nm"),
+            EngineTorqueCurveFieldContent = new GUIContent("Engine Torque Curve", "Normalized torque curve of the engine"),
+            GearCountFieldContent = new GUIContent("Gear Count", "Number of forward gears"),
+            ReverseGearRatioFieldContent = new GUIContent("Reverse Gear Ratio", "Reverse gear's ratio"),
+            FinalDriveRatioFieldContent = new GUIContent("Final Drive Ratio", "Final drive's ratio");
+
+        private bool fieldsDirty;
+
+        private bool viewRawJson;
+
+        private void OnEnable()
+        {
+            specSheet = (CarSpecSheet)target;
+
+            specSheetData = specSheet.Data;
+
+            if (specSheetData.GearRatios == null) { specSheetData.GearRatios = new float[specSheetData.GearCount]; }
+
+            if(specSheetData.EngineTorqueCurveKeyframes == null || specSheetData.EngineTorqueCurveKeyframes.Length == 0)
+            {
+                engineTorqueCurve = new AnimationCurve(new Keyframe(0f, 0f), new Keyframe(0.5f, 1f), new Keyframe(1f, 0f));
+            }
+            else
+            {
+                engineTorqueCurve = new AnimationCurve(specSheetData.EngineTorqueCurveKeyframes);
+            }
+        }
+
+        public override void OnInspectorGUI()
+        {
+            using (var scope = new EditorGUI.ChangeCheckScope())
+            {
+                specSheetData.Name = EditorGUILayout.TextField(NameFieldContent, specSheetData.Name);
+
+                specSheetData.Description = EditorGUILayout.TextField(DescriptionFieldContent, specSheetData.Description);
+
+                specSheetData.Make = EditorGUILayout.TextField(MakeFieldContent, specSheetData.Make);
+
+                specSheetData.Model = EditorGUILayout.TextField(ModelFieldContent, specSheetData.Model);
+
+                specSheetData.Year = EditorGUILayout.IntField(YearFieldContent, specSheetData.Year);
+
+                specSheetData.Weight = EditorGUILayout.FloatField(WeightFieldContent, specSheetData.Weight);
+
+                specSheetData.TopSpeed = EditorGUILayout.FloatField(TopSpeedFieldContent, specSheetData.TopSpeed);
+
+                specSheetData.MaxEngineTorque = EditorGUILayout.FloatField(MaxEngineTorqueFieldContent, specSheetData.MaxEngineTorque);
+
+                using (var engineTorqueCurveChangedScope = new EditorGUI.ChangeCheckScope())
+                {
+                    engineTorqueCurve = EditorGUILayout.CurveField(EngineTorqueCurveFieldContent, engineTorqueCurve, GUILayout.Height(96));
+
+                    if (engineTorqueCurveChangedScope.changed)
+                    {
+                        specSheetData.EngineTorqueCurveKeyframes = engineTorqueCurve.keys;
+                    }
+                }
+
+                using (var gearCountChangedScope = new EditorGUI.ChangeCheckScope())
+                {
+                    specSheetData.GearCount = EditorGUILayout.IntField(GearCountFieldContent, specSheetData.GearCount);
+
+                    if (gearCountChangedScope.changed)
+                    {
+                        System.Array.Resize(ref specSheetData.GearRatios, specSheetData.GearCount);
+                    }
+                }
+
+                for(int i = 0; i < specSheetData.GearCount; i++)
+                {
+                    specSheetData.GearRatios[i] = EditorGUILayout.Slider($"Gear {i + 1} Ratio", specSheetData.GearRatios[i], 0.1f, 10f);
+                }
+
+                specSheetData.ReverseGearRatio = EditorGUILayout.FloatField(ReverseGearRatioFieldContent, specSheetData.ReverseGearRatio);
+
+                specSheetData.FinalDriveRatio = EditorGUILayout.FloatField(FinalDriveRatioFieldContent, specSheetData.FinalDriveRatio);
+
+                if (scope.changed)
+                {
+                    fieldsDirty = true;
+                }
+            }
+
+            if(viewRawJson = EditorGUILayout.Foldout(viewRawJson, "Raw Json", true))
+            {
+                EditorGUILayout.TextArea(specSheet.RawJsonData);
+            }
+
+            using (new EditorGUI.DisabledScope(!fieldsDirty))
+            {
+                if(GUILayout.Button("Save Spec Sheet", GUILayout.Height(32f)))
+                {
+                    specSheet.Data = specSheetData;
+
+                    AssetDatabase.SaveAssets();
+
+                    fieldsDirty = false;
+                }
+            }
+        }
+    }
+}
---
+++ Packages/com.varneon.vudon.vehicles-base/Runtime/ScriptableObjects/CarSpecSheet.cs
@@ -0,0 +1,94 @@
+﻿using UnityEngine;
+using Newtonsoft.Json;
+
+namespace Varneon.VUdon.VehiclesBase.DataPresets
+{
+    [CreateAssetMenu(menuName = "VUdon - Vehicles/Data Presets/Car Spec Sheet", fileName = "NewCarSpecSheet.asset", order = 100)]
+    public class CarSpecSheet : ScriptableObject
+    {
+        public CarSpecSheetData Data
+        {
+            get
+            {
+                return string.IsNullOrEmpty(rawJsonData) ? new CarSpecSheetData() : JsonConvert.DeserializeObject<CarSpecSheetData>(rawJsonData);
+            }
+            set
+            {
+                rawJsonData = JsonConvert.SerializeObject(value, Formatting.Indented);
+            }
+        }
+
+        public string RawJsonData => rawJsonData;
+
+        [SerializeField]
+        internal string rawJsonData;
+
+        public class CarSpecSheetData
+        {
+            /// <summary>
+            /// Name of the car preset
+            /// </summary>
+            public string Name = string.Empty;
+
+            /// <summary>
+            /// Description of the car
+            /// </summary>
+            public string Description = string.Empty;
+
+            /// <summary>
+            /// Make of the car
+            /// </summary>
+            public string Make = string.Empty;
+
+            /// <summary>
+            /// Car model
+            /// </summary>
+            public string Model = string.Empty;
+
+            /// <summary>
+            /// Production year of the car
+            /// </summary>
+            public int Year = 2022;
+
+            /// <summary>
+            /// Default operational weight of the car
+            /// </summary>
+            public float Weight = 1750f;
+
+            /// <summary>
+            /// Top speed of the car
+            /// </summary>
+            public float TopSpeed = 200f;
+
+            /// <summary>
+            /// Maximum torque of the engine in Nm
+            /// </summary>
+            public float MaxEngineTorque = 400f;
+
+            /// <summary>
+            /// AnimationCurve keyframes for describing the engine's torque curve
+            /// </summary>
+            public Keyframe[] EngineTorqueCurveKeyframes;
+
+            /// <summary>
+            /// Number of forward gears
+            /// </summary>
+            public int GearCount = 6;
+
+            /// <summary>
+            /// Ratios of each forward gear
+            /// </summary>
+            public float[] GearRatios = new float[] { 5.5f, 3.5f, 2f, 1.35f, 1f, 0.8f };
+
+            /// <summary>
+            /// Reverse gear's ratio
+            /// </summary>
+            public float ReverseGearRatio = 5f;
+
+            /// <summary>
+            /// Final drive's ratio
+            /// </summary>
+            public float FinalDriveRatio = 4f;
+        }
+    }
+}
