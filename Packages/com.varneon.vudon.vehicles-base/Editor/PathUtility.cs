ae21617db052f2636ccb2e8df8f2352f4045019a
Varneon <me@varneon.com>
2022-12-06 16:43:47 -0800

    Add PathUtility class

---
+++ Packages/com.varneon.vudon.vehicles-base/Editor/PathUtility.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using UnityEngine;
+
+namespace Varneon.VUdon.VehiclesBase.Editor
+{
+    public static class PathUtility
+    {
+        /// <summary>
+        /// Converts full path to one relative to the project
+        /// </summary>
+        /// <param name="path">Full path pointing inside the project</param>
+        /// <returns>Path relative to the project</returns>
+        /// <exception cref="ArgumentException"/>
+        public static string ConvertToRelativePath(string path)
+        {
+            // If string is null or empty, throw an exception
+            if (string.IsNullOrEmpty(path)) { throw new ArgumentException("Invalid path!", nameof(path)); }
+
+            // Get the 'Assets' directory
+            string assetsDirectory = Application.dataPath;
+
+            // Get the project's root directory (Trim 'Assets' from the end of the path)
+            string projectDirectory = assetsDirectory.Substring(0, assetsDirectory.Length - 6);
+
+            // Ensure that the path is the full path
+            path = Path.GetFullPath(path);
+
+            // Replace backslashes with forward slashes
+            path = path.Replace('\\', '/');
+
+            // If path doesn't point inside the project, throw an exception
+            if (!path.StartsWith(projectDirectory)) { throw new ArgumentException("Path is not located in this project!", nameof(path)); }
+
+            // Return a path relative to the project
+            return path.Replace(projectDirectory, string.Empty);
+        }
+    }
+}
