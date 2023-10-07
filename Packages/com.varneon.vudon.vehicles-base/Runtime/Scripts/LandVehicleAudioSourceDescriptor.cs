7f3f5471829b47579ac032edf954c9b82f77c351
Varneon <me@varneon.com>
2023-10-07 02:18:51 -0700

    Add audio source descriptor

---
+++ Packages/com.varneon.vudon.vehicles-base/Runtime/Scripts/LandVehicleAudioSourceDescriptor.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using Varneon.VUdon.VehiclesBase.Interfaces;
+
+namespace Varneon.VUdon.VehiclesBase
+{
+    [RequireComponent(typeof(AudioSource))]
+    [DisallowMultipleComponent]
+    [AddComponentMenu("VUdon/Vehicles/Descriptors/Land Vehicle Audio Source")]
+    public class LandVehicleAudioSourceDescriptor : MonoBehaviour, IDestroyOnBuild
+    {
+        public enum AudioSourceType
+        {
+            Engine,
+            EngineSFX,
+            InteriorSFX,
+            RoadNoise,
+            SkidNoise,
+            Other
+        }
+
+        public AudioSourceType Type => type;
+
+        [SerializeField]
+        private AudioSourceType type;
+
+        [SerializeField]
+        private bool lowpassEnabled;
+
+        [SerializeField, Range(0f, 22000f)]
+        private float lowpassFrequency = 22000f;
+    }
+
+#if UNITY_EDITOR && !COMPILER_UDONSHARP
+    [UnityEditor.CustomEditor(typeof(LandVehicleAudioSourceDescriptor))]
+    public class LandVehicleAudioSourceDescriptorEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            UnityEditor.EditorGUILayout.HelpBox("This descriptor is used to define an audio source for a land vehicle.", UnityEditor.MessageType.Info);
+        }
+    }
+#endif
+}
