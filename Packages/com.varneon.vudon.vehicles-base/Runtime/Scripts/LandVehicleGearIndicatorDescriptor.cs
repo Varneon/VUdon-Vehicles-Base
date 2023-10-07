3c0102c2f25bde4a58f1e65989621f44753cde49
Varneon <me@varneon.com>
2023-10-07 01:20:13 -0700

    Add gear indicator descriptor

---
+++ Packages/com.varneon.vudon.vehicles-base/Runtime/Scripts/LandVehicleGearIndicatorDescriptor.cs
@@ -0,0 +1,22 @@
+﻿using TMPro;
+using UnityEngine;
+using Varneon.VUdon.VehiclesBase.Interfaces;
+
+namespace Varneon.VUdon.VehiclesBase
+{
+    [RequireComponent(typeof(TextMeshPro))]
+    [DisallowMultipleComponent]
+    [AddComponentMenu("VUdon/Vehicles/Descriptors/Land Vehicle Gear Indicator")]
+    public class LandVehicleGearIndicatorDescriptor : MonoBehaviour, IDestroyOnBuild { }
+
+#if UNITY_EDITOR && !COMPILER_UDONSHARP
+    [UnityEditor.CustomEditor(typeof(LandVehicleGearIndicatorDescriptor))]
+    public class LandVehicleGearIndicatorDescriptorEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            UnityEditor.EditorGUILayout.HelpBox("This descriptor is used to define a gear indicator text for a land vehicle.", UnityEditor.MessageType.Info);
+        }
+    }
+#endif
+}
