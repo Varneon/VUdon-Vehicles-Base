b149bd3fa050fb7854bf3b8a9a868871d7272843
Varneon <me@varneon.com>
2023-10-07 03:01:28 -0700

    Add vehicle visibility sensor

---
+++ Packages/com.varneon.vudon.vehicles-base/Runtime/Udon Programs/VehicleVisibilitySensor.cs
@@ -0,0 +1,31 @@
+﻿using UdonSharp;
+using Varneon.VUdon.VehiclesBase.Abstract;
+using Varneon.VUdon.VisibilitySensors.Abstract;
+using UnityEngine;
+using VRC.Udon;
+
+namespace Varneon.VUdon.VehiclesBase
+{
+    /// <summary>
+    /// Visibility sensor for vehicles
+    /// </summary>
+    [UdonBehaviourSyncMode(BehaviourSyncMode.None)]
+    public class VehicleVisibilitySensor : VisibilitySensor
+    {
+        /// <summary>
+        /// Root vehicle which this sensor is attached to
+        /// </summary>
+        [SerializeField, HideInInspector]
+        internal UdonBehaviour vehicle;
+
+        public override void _onBecameVisible()
+        {
+            vehicle.SendCustomEvent(nameof(Vehicle.SetVehicleStateVisible));
+        }
+
+        public override void _onBecameInvisible()
+        {
+            vehicle.SendCustomEvent(nameof(Vehicle.SetVehicleStateInvisible));
+        }
+    }
+}
