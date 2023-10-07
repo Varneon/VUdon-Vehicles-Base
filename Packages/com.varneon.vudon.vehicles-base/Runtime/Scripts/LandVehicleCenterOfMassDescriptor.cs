89fff2501538a85491a9690ca66011057663b018
Varneon <me@varneon.com>
2023-10-07 01:21:53 -0700

    Add center of mass descriptor

---
+++ Packages/com.varneon.vudon.vehicles-base/Runtime/Scripts/LandVehicleCenterOfMassDescriptor.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Varneon.VUdon.VehiclesBase.Abstract;
+
+namespace Varneon.VUdon.VehiclesBase
+{
+    [DisallowMultipleComponent]
+    [AddComponentMenu("VUdon/Vehicles/Descriptors/Center Of Mass Descriptor")]
+    public class LandVehicleCenterOfMassDescriptor : LandVehicleObjectDescriptor { }
+
+#if UNITY_EDITOR && !COMPILER_UDONSHARP
+    [UnityEditor.CustomEditor(typeof(LandVehicleCenterOfMassDescriptor))]
+    public class LandVehicleCenterOfMassDescriptorEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            UnityEditor.EditorGUILayout.HelpBox("This descriptor is used to define the center of mass for a land vehicle.", UnityEditor.MessageType.Info);
+        }
+    }
+#endif
+}
