18bab83be4a9d4d3dcd3eaa0085252f10c81db35
Varneon <me@varneon.com>
2023-10-07 01:21:12 -0700

    Add object descriptor

---
+++ Packages/com.varneon.vudon.vehicles-base/Runtime/Scripts/Abstract/LandVehicleObjectDescriptor.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using Varneon.VUdon.VehiclesBase.Interfaces;
+
+namespace Varneon.VUdon.VehiclesBase.Abstract
+{
+    [DisallowMultipleComponent]
+    public abstract class LandVehicleObjectDescriptor : MonoBehaviour, IDestroyOnBuild { }
+}
